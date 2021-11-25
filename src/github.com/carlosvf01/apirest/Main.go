39d5062319354ec62dcd78c9d970515e0a4119b7
CarlosVF01 <carlosfornesvela@gmail.com>
2021-11-25 11:26:15 +0100

    New Get Requests and Routing changes
    Added 2 new GET requests:
    
    /employees/{name} that finds all employees that match the name passed
    
    /employees/{id} that finds the employee with that ID
    
    Regarding the routing changes I added Mux to replace the raw HTTP Api

--- src/github.com/carlosvf01/apirest/Main.go
+++ src/github.com/carlosvf01/apirest/Main.go
@@ -5,11 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 var db *sql.DB
+
 type Employee struct {
 	ID   int64
 	Name string
@@ -17,16 +20,49 @@ type Employee struct {
 }
 
 func main() {
-	setupDatabaseConnection()
+	SetupDatabaseConnection()
 	handleRequests()
 }
 // Function that handles the endpoints that are accessible
 func handleRequests() {
-	http.HandleFunc("/employees", returnAllEmployees)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.Headers().HeadersRegexp("Content-Type", "application/(text|json)")
+
+	router.HandleFunc("/employees/{name:[a-zA-Z]+}", EmployeeByNameHandler).Methods("GET")
+	router.HandleFunc("/employees/{id:[0-9]+}", EmployeeByIdHandler).Methods("GET")
+	router.HandleFunc("/employees", GetAllEmployeesHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func EmployeeByIdHandler(w http.ResponseWriter, r *http.Request)  {
+	vars := mux.Vars(r)
+	fmt.Println("Endpoint Hit: GetEmployeeById")
+	id, _ := strconv.ParseInt(vars["id"], 10, 32)
+	employee := GetEmployeesByIdQuery(id)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(employee)
+}
+func GetAllEmployeesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: GetAllEmployees")
+
+	employees := GetAllEmployeesQuery()
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(employees)
+}
+func EmployeeByNameHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println("Endpoint Hit: GetEmployeeByName")
+
+	employees := GetEmployeesByNameQuery(vars["name"])
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(employees)
+
+
 }
 // Function that queries for employees that have the specified name
-func findEmployeesByNameQuery(name string) []Employee {
+func GetEmployeesByNameQuery(name string) []Employee {
 	// An employees slice to hold data from returned rows.
 	var employees []Employee
 
@@ -37,16 +73,13 @@ func findEmployeesByNameQuery(name string) []Employee {
 	for rows.Next() {
 		var employee Employee
 
+		rows.Scan(&employee.ID, &employee.Name, &employee.Age)
 		employees = append(employees, employee)
 	}
 
 	return employees
 }
-func returnAllEmployees(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllEmployees")
-	json.NewEncoder(w).Encode(getAllEmployeesQuery())
-}
-func getAllEmployeesQuery() []Employee {
+func GetAllEmployeesQuery() []Employee {
 	var employees []Employee
 
 	rows, _ := db.Query("SELECT * FROM emp")
@@ -62,8 +95,24 @@ func getAllEmployeesQuery() []Employee {
 
 	return employees
 }
+func GetEmployeesByIdQuery(id int64) Employee {
+	var employee Employee
+
+	rows, _ := db.Query("SELECT * FROM emp WHERE id = ?", id)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var dbEmployee Employee
+		rows.Scan(&dbEmployee.ID, &dbEmployee.Name, &dbEmployee.Age)
+
+		employee = dbEmployee
+	}
+
+	return employee
+}
 // Function that sets up the mysql database connection
-func setupDatabaseConnection() {
+func SetupDatabaseConnection() {
 	// Capture connection properties.
 	cfg := mysql.Config{
 		User:                 os.Getenv("DBUSER"),
